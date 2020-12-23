2dde7acfe8f5f09cac9b7af0f309d5244bca5a91
Gili Tzabari <cowwoc2020@gmail.com>
2020-12-23 12:27:16 -0500

    CMake's macOS binaries have changed filenames.

--- cmake-common/src/main/java/com/googlecode/cmakemavenproject/OperatingSystem.java
+++ cmake-common/src/main/java/com/googlecode/cmakemavenproject/OperatingSystem.java
@@ -141,7 +141,7 @@ public final class OperatingSystem
 				throw new UnsupportedOperationException("Unsupported architecture: " + architecture);
 			case MAC:
 				if (architecture == Architecture.X86_64)
-					return "Darwin-x86_64.tar.gz";
+					return "universal.tar.gz";
 				throw new UnsupportedOperationException("Unsupported architecture: " + architecture);
 			case WINDOWS:
 				if (architecture == Architecture.X86_64)
